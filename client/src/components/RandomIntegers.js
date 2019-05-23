9248841bff8e3688b55b05bafb444c3fa282b674
tae-lim <taelim92@gmail.com>
2019-05-23 15:25:50 -0700

    Fetch at '/api/number' every 2000 ms and update date after request

--- client/src/components/RandomIntegers.js
+++ client/src/components/RandomIntegers.js
@@ -4,28 +4,33 @@ class RandomIntegers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      integers: [31, 23, 5, 13, 99, 13, 16, 75, 44, 56, 31]
+      integers: [],
+      date: ''
     }
-    this.fetchRandomInteger = this.fetchRandomInteger.bind(this);
-    this.updateIntegers = this.updateIntegers.bind(this);
   }
 
   componentDidMount() {
-    this.fetchRandomInteger();
+    this.randomIntegerTimer = setInterval(() => {
+      this.fetchRandomInteger();
+    }, 2000);
+  }
+
+  getCurrentDate() {
+    const date = new Date();
+    this.setState({ date }, () => {console.log(this.state.date)});
   }
 
   updateIntegers(integer) {
     const integers = this.state.integers;
     integers.push(integer);
-    this.setState({
-      integers
-    }, () => {console.log(this.state.integers)});
+    this.setState({ integers }, () => {console.log(this.state.integers)});
   }
 
   fetchRandomInteger() {
     fetch('/api/number')
       .then(response => response.text())
       .then(integer => this.updateIntegers(Number(integer)))
+      .then(() => this.getCurrentDate())
       .catch(err => console.log(err));
   }
 
