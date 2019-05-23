3cea4c435a7feeb2df9c4ef34ada0caf6bc3fb63
tae-lim <taelim92@gmail.com>
2019-05-23 14:37:04 -0700

    Setup handler in server for '/api/number'

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
+//Initialization
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 6060;
 
+//Helper Functions
+const rng = require('./randomNumberGenerator');
+
+//Middleware
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 
+//API
+app.get('/api/number', (req, res) => {
+  const number = rng.randomNumberGenerator();
+  res.send(`${number}`);
+});
+
+//Listen for connection
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
 });
